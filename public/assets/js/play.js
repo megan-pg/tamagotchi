42e2721f37632a962f97dee08996062dedbcb03d
Spazcool <douglas.wrightiii@gmail.com>
2020-07-06 16:50:23 -0400

    add clock & update endpoints to frontend

--- public/assets/js/play.js
+++ public/assets/js/play.js
@@ -5,12 +5,12 @@ $(async () => {
   const obj = getClientCreds();
   const userStr = obj.username;
   const animal = await getAnimal(obj, userStr);
-  populateAnimalStats(animal.msg, userStr);
+  populateAnimalStats(animal.msg[0]);
 });
 
 async function getAnimal(creds, user) {
   const url = window.location.href.split('/');
-  const animal = url[url.length - 1];
+  const animal = url[url.length - 1]; // todo not sure if there's a better way to go about this
 
   return $.ajax({
     url: `/api/users/${user}/${animal}`,
@@ -37,43 +37,88 @@ function getClientCreds() {
   return obj;
 }
 
-function populateAnimalStats(animal, user) {
-  console.log('populate')
-  console.log(animal)
-  console.log(user)
-  // todo these are here for demoing, will need to be added to the db
-  // const type = 'bird';
-  // const state = 'bored'
-  // const display = `<li class="waves-effect" id="${animal.name}" data-animal="${animal.uuid}" style="padding-bottom: 5px;">
-  //     <div class="valign-wrapper">
-  //         <img 
-  //         src="/assets/concept-art/${type}-tamagotchi/img/${type}_${state}.png"
-  //         style="max-width:80px; height: 80px;border-radius:50%;"
-  //         / >
-  //         <div class="title">
-  //             ${animal.name}<br>
-  //             <span>${animal.createdAt}</span>
-  //         </div>
-  //         <i class="material-icons ml-auto"><i class="${'fas fa-poop'}"></i></i>
-  //     </div>
-  //   </li>`;
+function populateAnimalStats(animal) {
+  const type = 'bird';
+  const state = 'bored';
+  const stats = Object.entries(animal).map(([key, val]) => `<li>${key}: ${val}</li>`).join('');
+  const display = `<div class="waves-effect" id="animalBox">
+      <div class="valign-wrapper">
+          <img 
+          src="/assets/concept-art/${type}-tamagotchi/img/${type}_${state}.png"
+          style="max-width:80px; height: 80px;border-radius:50%;"
+          / >
+          <div class="title">
+            <ul>
+              ${stats}
+            </ul>
+          </div>
+          <i class="material-icons ml-auto"><i class="${'fas fa-poop'}"></i></i>
+      </div>
+      <button type="button" id="clock">Update all Stats (clock test)</button>
+      <button type="button" id="feed">Update Stat: hunger</button>
+    </div>`;
+  $('#animalBox').remove();
+  $('#animal').append(display);
+// todo move event listeners outside of this function, the buttons need not be dynamic above 
+  $('#clock').click(async () => {
+    const obj = getClientCreds();
+    const userStr = obj.username;
+    await updateStats({ uuid: animal.uuid }, getClientCreds());
+    const dude = await getAnimal(obj, userStr);
+    populateAnimalStats(dude.msg[0]);
+  });
 
-  // $('#animal').append(display);
+  $('#feed').click(async () => {
+    const obj = getClientCreds();
+    const userStr = obj.username;
+    updateStat({ uuid: animal.uuid, action: 'hunger' }, getClientCreds());
+    const dude = await getAnimal(obj, userStr);
+    populateAnimalStats(dude.msg[0]);
+  });
+
 }
 
+async function updateStats(data, creds) {
+  return $.ajax({
+    url: '/api/animals/clock',
+    type: 'put',
+    data,
+    headers: {
+      authorization: creds.token,
+    },
+    dataType: 'json',
+  })
+    .then(async (result) => result)
+    .fail((result) => {
+      // todo add a toast here
+      console.log(result);
+    });
+}
+
+async function updateStat(data, creds) {
+  return $.ajax({
+    url: '/api/animals/update',
+    type: 'put',
+    data,
+    headers: {
+      authorization: creds.token,
+    },
+    dataType: 'json',
+  })
+    .then(async (result) => result)
+    .fail((result) => {
+      // todo add a toast here
+      console.log(result);
+    });
+}
 // todo might not need this one
-function validateInputs(obj) {
-  const inputs = Object.entries(obj).filter(([key, val]) => val.length === 0);
+// function validateInputs(obj) {
+//   const inputs = Object.entries(obj).filter(([key, val]) => val.length === 0);
 
-  if (inputs.length > 0) {
-    const required = inputs.map(([key, val]) => `${key}: is required.`);
-    return required;
-  }
+//   if (inputs.length > 0) {
+//     const required = inputs.map(([key, val]) => `${key}: is required.`);
+//     return required;
+//   }
 
-  return true;
-}
-// data from url, username & animal name
-// data from local storage usuer uuid
-// call db for current animal status
-// push status to view
-// 
+//   return true;
+// }
