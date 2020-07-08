6e19c55102c9426684c362d04d467d9d6cc9dc08
sehouse <sehouse@umich.edu>
2020-07-07 20:24:46 -0400

    Inclusion of 2 missing sprite sheets.

--- public/assets/js/animation.js
+++ public/assets/js/animation.js
@@ -1,61 +1,42 @@
-//Basic animation code, to be inserted into the html
-//<div id="demo">
+// Basic animation code, to be inserted into the html
+// <div id="demo">
 //  <p id="image" onmouseover="animateScript()" onmouseout="stopAnimate()">
-
 //  </p>
-//</div>
+// </div>
 
-let tID; //we will use this variable to clear the setInterval()
-
+let tID; // we will use this variable to clear the setInterval()
 const stopAnimate = () => {
-    clearInterval(tID);
-} //end of stopAnimate()
-
+  clearInterval(tID);
+}; // end of stopAnimate()
 
 const eggHatching = () => {
-
-    let position = 0; //start position for the image slicer
-    const interval = 500; //500 ms of interval for the setInterval()
-    const diff = 640; //diff as a variable for position offset
-
-    tID = setInterval(() => {
-
-        document.getElementById("eggHatch").style.backgroundPosition =
-            `-${position}px 0px`;
-        //Template literal to insert the variable "position"
-
-        if (position < 4480) {
-            position = position + diff;
-        }
-        //we increment the position by 640 each time
-        else {
-            position = 0;
-        }
-        //reset the position to 0px, once position exceeds 4480px
-
-    }, interval); //end of setInterval
-} //end of animateScript()
+  let position = 0; // start position for the image slicer
+  const interval = 500; // 500 ms of interval for the setInterval()
+  const diff = 640; // diff as a variable for position offset
+  tID = setInterval(() => {
+    document.getElementById("eggHatch").style.backgroundPosition = `-${position}px 0px`;
+    // Template literal to insert the variable "position"
+    if (position < 4480) {
+      position += diff;
+    } else { // we increment the position by 640 each time
+      position = 0;
+    }
+    // reset the position to 0px, once position exceeds 4480px
+  }, interval); // end of setInterval
+}; // end of animateScript()
 
 const animateAnimalState = () => {
-
-    let position = 0; //start position for the image slicer
-    const interval = 500; //500 ms of interval for the setInterval()
-    const diff = 640; //diff as a variable for position offset
-
-    tID = setInterval(() => {
-
-        document.getElementById("image").style.backgroundPosition =
-            `-${position}px 0px`;
-        //Template literal to insert the variable "position"
-
-        if (position < 1280) {
-            position = position + diff;
-        }
-        //we increment the position by 640 each time
-        else {
-            position = 0;
-        }
-        //reset the position to 0px, once position exceeds 4480px
-
-    }, interval); //end of setInterval
-} //end of animateScript()
+  let position = 0; // start position for the image slicer
+  const interval = 500; // 500 ms of interval for the setInterval()
+  const diff = 640; // diff as a variable for position offset
+  tID = setInterval(() => {
+    document.getElementById("image").style.backgroundPosition = `-${position}px 0px`;
+    // Template literal to insert the variable "position"
+    if (position < 1280) {
+      position += diff;
+    } else { // we increment the position by 640 each time
+      position = 0;
+    }
+    // reset the position to 0px, once position exceeds 4480px
+  }, interval); // end of setInterval
+}; // end of animateScript()
